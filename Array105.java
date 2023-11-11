0c0fe50a6ac65bd740b01d1cb8d9fdfbc2fc55e6
karinaika <karinaikaindasa@gmail.com>
2023-11-11 13:18:00 +0700

    up

---
+++ Array105.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Array105{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        String[] menu = new String[5];
+        int[] harga = new int[5];
+
+        // menambah data menu
+        menu[0] = "Mie Ayam";
+        menu[1] = "Nasi Goreng";
+        menu[2] = "Mie Goreng";
+        menu[3] = "Sate Ayam";
+        menu[4] = "Es Teh";
+
+        
+    }
+}
